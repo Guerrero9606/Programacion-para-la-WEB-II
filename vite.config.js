ebdfaab94b589b6c72bab31b16788653e47c3c7b
Edgar Guerrero <edgarguerrero37@gmail.com>
2026-08-01 12:09:36 -0500

    Inclusion del allowedHosts en vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
   },
+  server: {
+    allowedHosts: ["ministers-humanitarian-strict-guitars.trycloudflare.com"]
+  }
 });
